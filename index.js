4457e5e2f8424454209b1165ff61991c44445d50
Marjan Stojanov <marjanstojanov90@gmail.com>
2023-11-07 19:55:05 +0100

    Add files to git

---
+++ index.js
@@ -0,0 +1,47 @@
+// https://github.com/vitejs/vite/issues/544
+// Extracted from https://unpkg.com/vue@3.3.8/dist/vue.esm-browser.js
+
+import fs from 'fs'
+
+export default function AppVue(version) {
+  // We will use the dev vue url from a CDN to extract all the export names
+  // and provide them as ESM module export from the global Vue instance.
+  // The main app file uses the same CDN as well.
+  const devVueUrl = `https://unpkg.com/vue@${version}/dist/vue.global.js`
+  const vueAliasTmpFilePath = `./.app-vue.js`
+
+  return {
+    name: 'vite-plugin-app-vue',
+
+    config: () => ({
+      resolve: {
+        alias: [{ find: 'vue', replacement: vueAliasTmpFilePath }],
+      },
+    }),
+
+    async buildStart() {
+      const src = await (await fetch(devVueUrl)).text()
+
+      const regex = /exports\.(\w+)/gm
+      const uniqueExports = new Set()
+
+      let content = ''
+
+      let match
+
+      while ((match = regex.exec(src)) !== null) {
+        uniqueExports.add(match[1])
+      }
+
+      uniqueExports.forEach(name => {
+        content += `export const ${name} = Vue.${name};\n`
+      })
+
+      fs.writeFileSync(vueAliasTmpFilePath, content)
+    },
+
+    buildEnd() {
+      fs.unlinkSync(vueAliasTmpFilePath)
+    },
+  }
+}
