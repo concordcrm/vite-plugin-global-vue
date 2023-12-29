51b7c63dca40b7d36da33f0ef3f53d982a7c8802
Marjan Stojanov <marjanstojanov90@gmail.com>
2023-12-29 15:20:56 +0100

    get version from config file

--- index.js
+++ index.js
@@ -2,18 +2,29 @@
 
 import fs from 'fs'
 
-export const versionMatchRegex =
-  /'vue_version'\s*=>\s*'((\d+)\.(\d+)\.(\d+)(?:-([\da-zA-Z-]+(?:\.[\da-zA-Z-]+)*))?(?:\+([\da-zA-Z-]+(?:\.[\da-zA-Z-]+)*))?)'/
-
-export default function AppVue(version) {
-  // We will use the dev Vue url from a CDN to extract all the export names
-  // and provide them as ESM module export from the global Vue instance.
-  // The main app file uses the same CDN as well.
-  const devVueUrl = `https://unpkg.com/vue@${version}/dist/vue.global.js`
+export default function AppVue(config) {
   const exportMatchRegex = /exports\.(\w+)/gm
-
   const vueAliasTmpFileName = `.vue.alias.js`
-  let vueAliasTmpPath = null
+  let vueAliasTmpPath = ''
+
+  let version = ''
+  let rootPath = ''
+
+  if (typeof config === 'string') {
+    version = config
+  } else if (typeof config === 'object') {
+    version = config.version
+    rootPath = config.rootPath || ''
+  }
+
+  if (!version) {
+    version = getVersionFromConfigFile(rootPath)
+  }
+
+  // We will use the dev Vue URL from a CDN to extract all the export names
+  // and provide them as ESM module export from the global Vue instance.
+  // Note that the main app file uses the same CDN as well.
+  const devVueUrl = `https://unpkg.com/vue@${version}/dist/vue.global.js`
 
   return {
     name: 'vite-plugin-global-vue',
@@ -55,5 +66,24 @@ export default function AppVue(version) {
     buildEnd() {
       fs.unlinkSync(vueAliasTmpPath)
     },
+  }
+
+  function getVersionFromConfigFile(rootPath) {
+    // semver matching support
+    const versionMatchRegex =
+      /'vue_version'\s*=>\s*'((\d+)\.(\d+)\.(\d+)(?:-([\da-zA-Z-]+(?:\.[\da-zA-Z-]+)*))?(?:\+([\da-zA-Z-]+(?:\.[\da-zA-Z-]+)*))?)'/
+
+    try {
+      const formattedRootPath =
+        rootPath && rootPath.endsWith('/') ? rootPath.slice(0, -1) : rootPath
+
+      const filePath = `${
+        formattedRootPath ? formattedRootPath + '/' : ''
+      }config/app.php`
+
+      return fs.readFileSync(filePath, 'utf8').match(versionMatchRegex)[1]
+    } catch (error) {
+      throw new Error('Failed to read config/app.php file.')
+    }
   }
 }
